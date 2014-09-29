42a552ebd280231f60c959e2e8f9057a523533ed
Havard Grimelid <hgrimelid@gmail.com>
2014-09-29 20:23:50 +0200

    Add basic markup for posts listing.

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
 <?php get_header(); ?>
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-  <?php the_title(); ?>
-  <?php the_content(); ?>
-<?php endwhile; ?>
-<?php else: ?>
-<?php endif; ?>
+<div class="posts">
+  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <div class="post">
+      <h1 class="post-title">
+        <a href="<?php the_permalink(); ?>">
+          <?php the_title(); ?>
+        </a>
+      </h1>
 
+      <span class="post-date"><?php the_date(); ?></span>
+
+      <?php the_content(); ?>
+    </div>
+
+  <?php endwhile; ?>
+  <?php else: ?>
+  <?php endif; ?>
+
+</div>
 <?php get_footer(); ?>
