1a51b0aecbefdcdf0c7c35c207d02713e81dc123
Havard Grimelid <hgrimelid@gmail.com>
2014-09-29 21:34:43 +0200

    Remove link.

--- single.php
+++ single.php
@@ -1,11 +1,7 @@
 <?php get_header(); ?>
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <div class="post">
-      <h1 class="post-title">
-        <a href="<?php the_permalink(); ?>">
-          <?php the_title(); ?>
-        </a>
-      </h1>
+      <h1 class="post-title"><?php the_title(); ?></h1>
 
       <span class="post-date"><?php the_date(); ?></span>
 
